be96fea0df0b40b3c9449d5c24f1e0e9e0157d9a
IgorNatann <Igornatan4@gmail.com>
2025-09-25 17:01:38 -0300

    Exercicio 01 Inserindo Dados Tabela Vendedores

---
+++ Curso_1_MSS22_conhecendo_SQL/Aula_01/INSERT_DESAFIO.sql
@@ -0,0 +1,9 @@
+-- INSERT TABELA DE VENDEDORES 
+INSERT INTO [TABELA_VENDEDORES] VALUES
+('00236', 'Claúdia Morais', '8.3'),
+('00237', 'Marcela Ferreira', '9.1'),
+('00238', 'Márcio Almeida', '8.5'),
+('00239', 'Igor Natan', '10.9'),
+('00240', 'Ana Beatriz', '7.1');
+
+SELECT * FROM TABELA_VENDEDORES;

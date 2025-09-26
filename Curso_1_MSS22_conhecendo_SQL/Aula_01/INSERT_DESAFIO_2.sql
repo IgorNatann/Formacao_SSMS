071eea859070f896d617e37b8d33ee85568726ad
IgorNatann <Igornatan4@gmail.com>
2025-09-26 14:38:55 -0300

    Desafio inserindo Informações na Tabela de Clientes.

---
+++ Curso_1_MSS22_conhecendo_SQL/Aula_01/INSERT_DESAFIO_2.sql
@@ -0,0 +1,20 @@
+INSERT INTO TABELA_CLIENTES
+    (CPF, NOME, RUA, COMPLEMENTO, BAIRRO, ESTADO, CEP, DATA_NASCIMENTO, IDADE, SEXO,LIMITE_CREDITO, VOLUME_MINIMO_COMPRA, PRIMEIRA_COMPRA)
+VALUES
+('1471156710', 'Erica Carvalho', 'R. Iriquitia', '', 'Jardins', 'SP', '80012212', '1990-09-01', 32, 'F', 1700000000, 24500, 0),
+('19290992743', 'Fernando Cavalcante', 'R. Dois de Fevereiro', '', 'Agua Santa', 'RJ', '22000000', '2000-02-12', 23, 'M', 1000000000, 20000, 1),
+('2600586709', 'Cesar Teixeira', 'Rua Conde de Bonfim', '', 'Tijuca', 'RJ', '22020001', '2000-03-12', 23, 'M', 1200000000, 22000, 0),
+('3623344710', 'Marcos Nogueira', 'Av. Pastor Martin Luther King Junior', '', 'Inhauma', 'RJ', '22002012', '1995-01-13', 28, 'M', 1100000000, 22000, 1),
+('492472718', 'Eduardo Jorge', 'R. Volta Grande', '', 'Tijuca', 'RJ', '22012002', '1994-07-19', 28, 'M', 750000000, 9500, 1),
+('50534475787', 'Abel Silva', 'Rua Humaita', NULL, 'Humaita', 'RJ', '220002120', '1995-09-11', 27, 'M', 1700000000.00, 26000, 0),
+('05576228758', 'Petra Oliveira', 'R. Benicio de Abreu', NULL, 'Lapa', 'SP', '881920290', '1995-11-14', 27, 'F', 700000000.00, 16000, 1),
+('05648641702', 'Paulo Cesar Mattos', 'Rua Helio Beltrao', NULL, 'Tijuca', 'RJ', '210020200', '1991-08-30', 31, 'M', 1200000000.00, 22000, 0), 
+('05840119709', 'Gabriel Araujo', 'R. Manuel de Oliveira', NULL, 'Santo Amaro', 'SP', '800102210', '1985-03-16', 38, 'M', 1400000000.00, 21000, 1),
+('07771579779', 'Marcelo Mattos', 'R. Eduardo Luís Lopes', NULL, 'Bras', 'SP', '882029120', '1992-03-25', 31, 'M', 1200000000.00, 20000, 1), 
+('08502682733', 'Valdeci da Silva', 'R. Srg. Edison de Oliveira', NULL, 'Jardins', 'SP', '821220200', '1995-10-07', 27, 'M', 1100000000.00, 19000, 0),
+('08719655770', 'Carlos Eduardo', 'Av. Gen. Guedes da Fontoura', NULL, 'Jardins', 'SP', '811920020', '1983-12-20', 39, 'M', 2000000000.00, 24000, 0),
+('09283760794', 'Edson Meilelles', 'R. Pinto de Azevedo', NULL, 'Cidade Nova', 'RJ', '220020020', '1995-10-07', 27, 'M', 1500000000.00, 25000, 1),
+('94387575700', 'Walber Lontra', 'R. Cel. Almeida', NULL, 'Piedade', 'RJ', '220002010', '1989-06-20', 33, 'M', 600000000.00, 12000, 1),
+('95939180787', 'Fabio Carvalho', 'R. dos Jacarandas da Peninsula', NULL, 'Barra da Tijuca', 'RJ', '220020200', '1992-01-05', 31, 'M', 900000000.00, 18000, 1);
+
+SELECT * FROM TABELA_CLIENTES;

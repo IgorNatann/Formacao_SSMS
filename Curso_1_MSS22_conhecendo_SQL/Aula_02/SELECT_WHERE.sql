e8e6eebd7c769604da879b4cd8d90522fc877eb6
IgorNatann <Igornatan4@gmail.com>
2025-09-26 15:59:13 -0300

    Aplicando filtro WHERE para selecionar clientes que realizaram a primeira compra

---
+++ Curso_1_MSS22_conhecendo_SQL/Aula_02/SELECT_WHERE.sql
@@ -0,0 +1,8 @@
+-- Aplicando Filtro WHERE.
+SELECT
+ [NOME] AS [NOME DO CLIENTE],
+ [ESTADO] AS [UF],
+ [PRIMEIRA_COMPRA]
+FROM [TABELA_CLIENTES]
+-- Filtrar apenas os clientes que realizaram a primeira compra.
+WHERE [PRIMEIRA_COMPRA] = 1;

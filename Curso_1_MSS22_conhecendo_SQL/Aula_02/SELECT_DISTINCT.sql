353be0c9d9e30597d000c6d1c0e19db9f24af4b0
IgorNatann <Igornatan4@gmail.com>
2025-09-26 15:46:12 -0300

    consulta para retornar registros duplicados e distintos da tabela TABELA_PRODUTOS

---
+++ Curso_1_MSS22_conhecendo_SQL/Aula_02/SELECT_DISTINCT.sql
@@ -0,0 +1,11 @@
+-- Retorna todos os registros, incluindo duplicados.
+SELECT
+    [SABOR]
+FROM
+    [TABELA_PRODUTOS];
+
+-- Retorna apenas registros distintos, eliminando duplicados.
+SELECT DISTINCT
+    [SABOR]
+FROM
+    [TABELA_PRODUTOS];

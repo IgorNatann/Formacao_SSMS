7dfcf65f799f89921eeaaabd3f3a31b9f315d4d4
IgorNatann <Igornatan4@gmail.com>
2025-09-29 17:27:06 -0300

    Desafio: Adicionando consulta SQL para listar as 10 primeiras vendas do dia 01/10/2017 na tabela NOTAS_FISCAIS.

---
+++ Curso_2_MSS22_consultas_avancadas_SQL/Aula_03/Desafios/SELECT_DATA.sql
@@ -0,0 +1,5 @@
+-- Tendo em vista os conteúdos trabalhados em aula, qual é o comando SQL para listar as 10 primeiras vendas do dia 01/10/2017?
+
+SELECT DISTINCT TOP 10 *
+FROM NOTAS_FISCAIS
+WHERE DATA_VENDA = '2017-10-01';

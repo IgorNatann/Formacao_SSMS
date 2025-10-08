5153a458cf4ef0f270a231cc97db15196f20f964
IgorNatann <Igornatan4@gmail.com>
2025-10-08 15:26:31 -0300

    docs: adicionando documentação ao arquivo

--- Curso_3_MSS22_manipulando_dados_SQL/Aula_02/Scripts/Desafios/UPDATE_TB_CLIENTES.sql
+++ Curso_3_MSS22_manipulando_dados_SQL/Aula_02/Scripts/Desafios/UPDATE_TB_CLIENTES.sql
@@ -1,36 +1,32 @@
--- Exercício:
+-- ================================================================
+-- Desafio | Curso 3 | Aula 04 | Atividade 5 | Reforco de volume por compartilhamento de bairro
+-- Objetivo: detectar clientes que dividem bairro com vendedores e aplicar
+-- aumento de 30% em VOLUME_COMPRA usando UPDATE com JOIN.
+-- ================================================================
 
--- Podemos observar que os vendedores possuem bairros associados a eles. Vamos aumentar em 30% o volume de compra dos clientes que possuem, em seus endereços, bairros onde os vendedores possuam escritórios.
-
--- Dica: vamos usar um UPDATE com FROM, usando o seguinte INNER JOIN:
-
+-- Passo 1: identificar clientes que moram em bairros atendidos por vendedores
 SELECT
     A.CPF,
     A.NOME,
-    A.VOLUME_COMPRA
-FROM
-    TB_CLIENTES A
-INNER JOIN
-    TB_VENDEDORES B
-ON A.[BAIRRO] = B.[BAIRRO]
+    A.VOLUME_COMPRA,
+    A.BAIRRO
+FROM TB_CLIENTES AS A
+INNER JOIN TB_VENDEDORES AS B
+    ON A.BAIRRO = B.BAIRRO;
 
-UPDATE
-    TB_CLIENTES
-SET
-    VOLUME_COMPRA = VOLUME_COMPRA * 1.30
-FROM
-    TB_CLIENTES A
-INNER JOIN
-    TB_VENDEDORES B
-ON
-    A.[BAIRRO] = B.[BAIRRO]
+-- Passo 2: aplicar o reajuste de 30% no volume de compra desses clientes
+UPDATE A
+SET A.VOLUME_COMPRA = A.VOLUME_COMPRA * 1.30
+FROM TB_CLIENTES AS A
+INNER JOIN TB_VENDEDORES AS B
+    ON A.BAIRRO = B.BAIRRO;
 
+-- Passo 3: validar o resultado apos o UPDATE
 SELECT
     A.CPF,
     A.NOME,
-    A.VOLUME_COMPRA
-FROM
-    TB_CLIENTES A
-INNER JOIN
-    TB_VENDEDORES B
-ON A.[BAIRRO] = B.[BAIRRO] 
+    A.VOLUME_COMPRA,
+    A.BAIRRO
+FROM TB_CLIENTES AS A
+INNER JOIN TB_VENDEDORES AS B
+    ON A.BAIRRO = B.BAIRRO;

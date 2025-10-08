fd01d8d38b85402d1cc89402a379c45f3510534b
IgorNatann <Igornatan4@gmail.com>
2025-10-08 15:39:52 -0300

    docs: adicionando documentação ao arquivo

--- Curso_3_MSS22_manipulando_dados_SQL/Aula_02/Scripts/MERGE.sql
+++ Curso_3_MSS22_manipulando_dados_SQL/Aula_02/Scripts/MERGE.sql
@@ -1,6 +1,14 @@
+-- ================================================================
+-- Curso 3 | Aula 04 | Atividade 06 | Sincronizando precos com MERGE
+-- Objetivo: comparar UPDATE tradicional com MERGE para alinhar precos da
+-- tabela interna a partir da base SUCOS_FRUTAS.
+-- ================================================================
+
+-- Passo 1: reajustar a tabela externa (referencia) para provocar diferenca
 UPDATE SUCOS_FRUTAS.dbo.TABELA_DE_PRODUTOS
 SET PRECO_DE_LISTA = PRECO_DE_LISTA * 1.20;
 
+-- Passo 2: visualizar como ficaram os precos apos o reajuste
 SELECT
     A.DESCRITOR        AS PRODUTO_TB_INTERNO,
     A.PRECO_LISTA      AS PRECO_TB_INTERNO,
@@ -10,19 +18,21 @@ FROM TB_PRODUTOS AS A
 INNER JOIN SUCOS_FRUTAS.dbo.TABELA_DE_PRODUTOS AS B
     ON A.CODIGO = B.CODIGO_DO_PRODUTO;
 
--- Estrutura de alteração utilizando UPDATE.
-UPDATE A SET A.PRECO_LISTA = B.PRECO_DE_LISTA 
-FROM PRODUTOS A 
-INNER JOIN SUCOS_FRUTAS.DBO.TABELA_DE_PRODUTOS B 
-ON A.CODIGO = B.CODIGO_DO_PRODUTO;
+-- Passo 3: exemplo de sincronizacao usando UPDATE com JOIN (abordagem classica)
+UPDATE A
+SET A.PRECO_LISTA = B.PRECO_DE_LISTA
+FROM TB_PRODUTOS AS A
+INNER JOIN SUCOS_FRUTAS.dbo.TABELA_DE_PRODUTOS AS B
+    ON A.CODIGO = B.CODIGO_DO_PRODUTO;
 
--- Estrutura utilizando MERGE com UPDATE.
-MERGE INTO TB_PRODUTOS A
-USING SUCOS_FRUTAS.DBO.TABELA_DE_PRODUTOS B
-ON A.CODIGO = B.CODIGO_DO_PRODUTO
+-- Passo 4: exemplo equivalente utilizando MERGE (WHEN MATCHED -> UPDATE)
+MERGE INTO TB_PRODUTOS AS A
+USING SUCOS_FRUTAS.dbo.TABELA_DE_PRODUTOS AS B
+    ON A.CODIGO = B.CODIGO_DO_PRODUTO
 WHEN MATCHED THEN
-UPDATE SET A.PRECO_LISTA = B.PRECO_DE_LISTA;
+    UPDATE SET A.PRECO_LISTA = B.PRECO_DE_LISTA;
 
+-- Passo 5: conferir se os precos estao sincronizados apos o MERGE
 SELECT
     A.DESCRITOR        AS PRODUTO_TB_INTERNO,
     A.PRECO_LISTA      AS PRECO_TB_INTERNO,

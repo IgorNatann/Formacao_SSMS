540c86c89c627d59689fb85930390bd2ecb12c50
IgorNatann <Igornatan4@gmail.com>
2025-10-08 17:21:09 -0300

    docs: adicionando documentação ao arquivo

--- Curso_3_MSS22_manipulando_dados_SQL/Aula_02/Scripts/Desafios/DELETE.sql
+++ Curso_3_MSS22_manipulando_dados_SQL/Aula_02/Scripts/Desafios/DELETE.sql
@@ -1,21 +1,33 @@
--- Exclua as notas fiscais (apenas o cabeçalho) cujos clientes tenham menos que 18 anos.
--- Dica: use consulta abaixo:
+-- ================================================================
+-- Desafio | Curso 3 | Aula 04 | Atividade 09 | Excluindo cabecalhos de vendas com clientes menores
+-- Objetivo: localizar notas fiscais ligadas a clientes com idade < 18
+-- e remover somente o cabecalho em TB_VENDAS usando DELETE com JOIN.
+-- ================================================================
 
-SELECT A.NUMERO FROM [TB_VENDAS] A
-INNER JOIN [TB_CLIENTES] B ON A.[CPF] = B.[CPF] 
-WHERE B.[IDADE] < 18
+-- Passo 1: identificar os numeros das notas associadas a clientes menores
+SELECT A.NUMERO
+FROM TB_VENDAS AS A
+INNER JOIN TB_CLIENTES AS B
+    ON A.CPF = B.CPF
+WHERE B.IDADE < 18;
 
--- E use uma sintaxe parecida com a usada no UPDATE com FROM.
+-- Passo 2: conferir os dados completos antes de excluir
+SELECT *
+FROM TB_CLIENTES AS A
+INNER JOIN TB_VENDAS AS B
+    ON A.CPF = B.CPF
+WHERE A.IDADE < 18;
 
-SELECT A.NUMERO FROM [TB_VENDAS] A
-INNER JOIN [TB_CLIENTES] B ON A.[CPF] = B.[CPF] 
-WHERE B.[IDADE] < 18;
-
-SELECT * FROM TB_CLIENTES A
-INNER JOIN TB_VENDAS B ON A.CPF = B.CPF
-WHERE IDADE < 18;
-
+-- Passo 3: excluir apenas o cabecalho das notas com clientes menores
 DELETE A
-FROM TB_VENDAS A
-INNER JOIN [TB_CLIENTES] B ON A.[CPF] = B.[CPF] 
-WHERE B.[IDADE] < 18;
+FROM TB_VENDAS AS A
+INNER JOIN TB_CLIENTES AS B
+    ON A.CPF = B.CPF
+WHERE B.IDADE < 18;
+
+-- Passo 4: validar que nao restaram registros das notas removidas
+SELECT *
+FROM TB_VENDAS AS A
+INNER JOIN TB_CLIENTES AS B
+    ON A.CPF = B.CPF
+WHERE B.IDADE < 18;

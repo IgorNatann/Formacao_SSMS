d94a8d985c02926c746f10aa94289b42d0258b8a
IgorNatann <Igornatan4@gmail.com>
2025-10-10 15:04:01 -0300

    refactor: organização estrutura de pasta, feat: exercicio declarando variaveis

---
+++ Curso_4_MSS22_conhecendo_TSQL/Aula_02/Desafios/VARIAVEIS.sql
@@ -0,0 +1,11 @@
+-- Para colocarmos em prática o que aprendemos até aqui, que tal exercitarmos em um desafio? Pensando nisso, propomos a criação de 4 variáveis com as características abaixo:
+
+-- Nome: Cliente - Tipo: Caracteres com 10 posições.
+-- Nome: Idade - Tipo: Inteiro.
+-- Nome: DataNascimento - Tipo: Data.
+-- Nome: Custo - Tipo: Número com casas decimais.
+
+DECLARE @CLIENTE VARCHAR(10);
+DECLARE @IDADE INT;
+DECLARE @DATANASCIMENTO DATE;
+DECLARE @CUSTO FLOAT;
